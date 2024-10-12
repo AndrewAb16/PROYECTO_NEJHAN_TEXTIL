5a8a825b4f099636ab4bf2397fcdb7e43b9ab324
JeanCGZ <cruz1212guzman@gmail.com>
2024-10-12 05:27:42 -0500

    Add files via upload
    En proceso

---
+++ NEJHAN_TEXTIL/CapaDatos/D_Proveedores.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using CapaEntidad;
+
+namespace CapaDatos
+{
+    internal class D_Proveedores
+    {
+        public DataTable Listado_pv(string cTexto)
+        {
+            SqlDataReader Resultado;
+            DataTable tabla = new DataTable();
+
+            SqlConnection sqlcon = new SqlConnection(Conexion.cadena);
+
+            try 
+            {
+                SqlCommand comando = new SqlCommand("Listado_ma", sqlcon);
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.Add("@cTexto", SqlDbType.VarChar).Value = cTexto;
+
+                sqlcon.Open();  // Abrimos la conexión
+                Resultado = comando.ExecuteReader();
+                tabla.Load(Resultado);  // Cargamos los resultados en el DataTable
+
+                return tabla;
+            }
+            catch (Exception ex)
+            {
+                // Manejo de excepciones
+                throw ex;
+            }
+            finally
+            {
+                // Cerramos la conexión si está abierta
+                if (sqlcon != null && sqlcon.State == ConnectionState.Open)
+                {
+                    sqlcon.Close();
+                }
+            }
+
+        }
+    }
+}
